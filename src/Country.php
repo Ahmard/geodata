47f26aab14483addb284af14e4f0b3f0478b09d8
ahmard <ahmard.mu@gmail.com>
2022-06-17 22:26:15 +0100

    Ability to get country info using it's phone code

--- src/Country.php
+++ src/Country.php
@@ -11,6 +11,18 @@ class Country
         return require dirname(__DIR__) . '/raw/countries.php';
     }
 
+    public static function getByPhoneCode(int $phoneCode): array
+    {
+        $countriesByPhoneCode = require dirname(__DIR__) . '/raw/countries-phone-codes-to-index.php';
+
+        if (!array_key_exists($phoneCode, $countriesByPhoneCode)) {
+            throw new InvalidArgumentException("Country with phone code '$phoneCode' does not exists.");
+        }
+
+        $countries = (require dirname(__DIR__) . '/raw/countries.php');
+        return $countries[$countriesByPhoneCode[$phoneCode]];
+    }
+
     public static function getStates(int $countryId): array
     {
         $filePath = dirname(__DIR__) . "/raw/states/c$countryId.php";
